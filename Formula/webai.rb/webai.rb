23885ea7db1568545d9f3e7c071d3ca4985e2034
Great Lake Web Design <greatlakewebdesign@gmail.com>
2024-10-20 17:09:54 -0400

    update class sha

--- Formula/webai.rb/webai.rb
+++ Formula/webai.rb/webai.rb
@@ -2,8 +2,8 @@ class Webai < Formula
   desc "A special CLI tool for WebAI"
   homepage "https://github.com/mdepree5/webai"
   url "https://github.com/mdepree5/webai/releases/download/webai/index-macos"
-  sha256 "19e945368e9bc541fc9ab3bc05a65f98b844f46100773317b12d9fcee72d318c  index-macos"
-  version "0.1.1"
+  sha256 "19e945368e9bc541fc9ab3bc05a65f98b844f46100773317b12d9fcee72d318c"
+  version "0.1.2"
 
   def install
     bin.install "index-macos" => "webai"
